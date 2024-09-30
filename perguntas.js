d02204c9ebf04a147d970422c5a57838548e3df2
Luana varela <146104091+luanavareladeoliveira@users.noreply.github.com>
2024-09-30 12:02:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Qual é o conceito de Programação?',
+    'é um processo de escrita, testes e manutenção de programas de computadores'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital dos Estados Unidos?',
+    'A capital dos Estados Unidos é Washington D.C'
+)
+
+criaCartao(
+    'Programação',
+    'Qual a função de um Arduino Uno?',
+    ''
+)
+
+criaCartao(
+    'Lingua espanhola',
+    'Como se diz Bom Dia em espanhol?',
+    'Bom Dia em Espanhol é buen día'
+)
